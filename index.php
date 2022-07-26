00234d811c54169b12317f6708b60761d2dd5261
Chanisco <chaniscoj@gmail.com>
2022-07-26 15:11:08 +0200

    php test

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
-echo "Hello! Welcome to our website.";
+if(isset($_GET["name"]) && isset($_GET["email"]) && isset($_GET["message"])) {
+    $name = htmlspecialchars($_GET["name"]);
+    $email = htmlspecialchars($_GET["email"]);
+    $message = htmlspecialchars($_GET["message"]);
+    
+
+    // Displaying a welcome message
+    echo "Hello $name, $email, $message ";
+    echo "<p>oops</p>"
+} else {
+    echo "<p>Hi there! Welcome to our website.</p>";
+}
 
 ?>
